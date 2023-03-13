1a893b70e6bf6b859feb0ffdfe660f96234ad3c9
vidg2004 <vidgpd09316@fpt.edu.vn>
2023-03-13 17:31:05 +0700

    init

--- lab 2/Bài 4/bai4.js
+++ lab 2/Bài 4/bai4.js
@@ -1,2 +1,18 @@
-let arr=["tue","thu","sat","nine"]; 
-arr.splice(1,3,"mon","we","fri","sun")
+let daysOfWeek = ["Tue", "Thu", "Sat", "Nine"];
+
+// Thêm các phần tử mới vào mảng
+daysOfWeek.push("Mon", "Wed", "Fri", "Sun");
+
+// Tìm index của phần tử "Nine" và xóa nó
+let index = daysOfWeek.indexOf("Nine");
+if (index !== -1) {
+  daysOfWeek.splice(index, 1);
+}
+
+// Sắp xếp mảng theo thứ tự đúng
+daysOfWeek.sort(function(a, b) {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  return days.indexOf(a) - days.indexOf(b);
+});
+
+console.log( + daysOfWeek); // ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
